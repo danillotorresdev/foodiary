0cc7f67c49792cadb33f7d641c3d471e5c5c56d5
danillotorresdev <danilloept@gmail.com>
2026-03-25 19:42:07 -0300

    feat(dependencies): update package.json and pnpm-lock.yaml with new libraries and versions
    - Added new dependencies: @gorhom/bottom-sheet, @react-navigation/native, @react-navigation/native-stack, react-native-gesture-handler, react-native-reanimated, and react-native-screens.
    - Updated App component to integrate GestureHandlerRootView and Navigation.
    - Refactored Greetings screen to utilize navigation and added SignInBottomSheet functionality.
    - Improved Button component interface for better type handling.

--- foodiaryapp/src/ui/App.tsx
+++ foodiaryapp/src/ui/App.tsx
@@ -1,27 +1,29 @@
 import {
+  HostGrotesk_400Regular,
+  HostGrotesk_500Medium,
+  HostGrotesk_600SemiBold,
+  useFonts,
+} from '@expo-google-fonts/host-grotesk';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Navigation } from '@app/navigation';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+export function App() {
+  const [isFontsLoaded] = useFonts({
     HostGrotesk_400Regular,
     HostGrotesk_500Medium,
     HostGrotesk_600SemiBold,
-    useFonts,
-  } from '@expo-google-fonts/host-grotesk';
-  
-  import { SafeAreaProvider } from 'react-native-safe-area-context';
-  import { Greetings } from './screens/Greetings';
-  
-  export function App() {
-    const [isFontsLoaded] = useFonts({
-      HostGrotesk_400Regular,
-      HostGrotesk_500Medium,
-      HostGrotesk_600SemiBold,
-    });
-  
-    if (!isFontsLoaded) {
-      return null;
-    }
-  
-    return (
-      <SafeAreaProvider>
-        <Greetings />
-      </SafeAreaProvider>
-    );
+  });
+
+  if (!isFontsLoaded) {
+    return null;
   }
+
+  return (
+    <GestureHandlerRootView>
+      <SafeAreaProvider>
+        <Navigation />
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
+  );
+}
