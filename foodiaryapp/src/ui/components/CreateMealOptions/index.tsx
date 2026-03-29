03ef2248ff0a60880fb5ebd5d57ebe7060186e36
danillotorresdev <danilloept@gmail.com>
2026-03-29 15:47:11 -0300

    feat(meals): enhance meal management with new features and services
    - Added support for audio and video file uploads in meal creation.
    - Introduced MealDetails screen for displaying detailed meal information.
    - Updated MealsService to include methods for fetching meals by ID and creating meals with file uploads.
    - Enhanced MealCard component to navigate to MealDetails.
    - Refactored meal data structure to include new properties for status and input type.
    - Updated CreateMealOptions component to handle audio and picture modals.
    - Improved button styles and added ripple effects for better user interaction.
    - Updated theme colors and button variants for a more cohesive design.

--- foodiaryapp/src/ui/components/CreateMealOptions/index.tsx
+++ foodiaryapp/src/ui/components/CreateMealOptions/index.tsx
@@ -1,25 +1,55 @@
 import { theme } from '@ui/styles/theme';
 import { CameraIcon, LucideIcon, MicIcon } from 'lucide-react-native';
+import { useState } from 'react';
 import { Platform, Pressable, View } from 'react-native';
 import { AppText } from '../AppText';
+import { AudioModal } from '../AudioModal';
+import { PictureModal } from '../PictureModal';
 import { styles } from './styles';
 
 interface ICreateMealOptionsProps {
   disabled?: boolean;
+  onCreate?: () => void;
 }
 
-export function CreateMealOptions({ disabled = false }: ICreateMealOptionsProps) {
+export function CreateMealOptions({ disabled = false, onCreate }: ICreateMealOptionsProps) {
+  const [
+    currentVisibleModal,
+    setCurrentVisibleModal,
+  ] = useState<null | 'audio' | 'picture'>(null);
+
+  function handleOpenModal(modal: 'audio' | 'picture') {
+    setCurrentVisibleModal(modal);
+  }
+
+  function handleCloseModal() {
+    setCurrentVisibleModal(null);
+  }
+
   return (
     <View style={styles.container}>
+      <AudioModal
+        visible={currentVisibleModal === 'audio'}
+        onClose={handleCloseModal}
+        onCreate={onCreate}
+      />
+      <PictureModal
+        visible={currentVisibleModal === 'picture'}
+        onClose={handleCloseModal}
+        onCreate={onCreate}
+      />
+
       <OptionButton
         icon={MicIcon}
         label="Áudio"
         disabled={disabled}
+        onPress={() => handleOpenModal('audio')}
       />
       <OptionButton
         icon={CameraIcon}
         label="Foto"
         disabled={disabled}
+        onPress={() => handleOpenModal('picture')}
       />
     </View>
   );
@@ -28,15 +58,17 @@ export function CreateMealOptions({ disabled = false }: ICreateMealOptionsProps)
 interface IOptionButtonProps {
   icon: LucideIcon;
   label: string;
+  onPress: () => void;
   disabled?: boolean;
 }
 
-export function OptionButton({ icon: Icon, label, disabled }: IOptionButtonProps) {
+export function OptionButton({ icon: Icon, label, disabled, onPress }: IOptionButtonProps) {
   return (
     <View style={styles.buttonWrapper}>
       <Pressable
+        onPress={onPress}
         disabled={disabled}
-        android_ripple={{ color: 'rgba(0, 0, 0, 0.1)' }}
+        android_ripple={{ color: 'rgba(0, 0, 0, 0.1)', foreground: true }}
         style={({ pressed }) => [
           styles.button,
           (disabled || (pressed && Platform.OS === 'ios')) && { opacity: 0.5 },
